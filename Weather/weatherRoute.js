9e4e71833aeeddfc7665880912b54ce06af7500f
hyeon-aa <66656181+hyeon-aa@users.noreply.github.com>
2021-06-24 18:49:28 +0900

    Delete Weather directory

--- Weather/weatherRoute.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-    const weather = require('./weatherController');
-    const jwtMiddleware = require('../../../config/jwtMiddleware');
-
-  app.get('/app/weather', weather.weather);
-    //app.post('/app/weather', weather.weather);
-};
